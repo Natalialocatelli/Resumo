72db5fd039a1df0782965be1929157b718903857
Natália Locatelli <natalialocatellib@gmail.com>
2022-05-12 11:26:48 -0300

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Natalia Locatelli Barbosa on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashcardsUITests/FlashcardsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FlashcardsUITestsLaunchTests.swift
+//  FlashcardsUITests
+//
+//  Created by Natalia Locatelli Barbosa on 12/05/22.
+//
+
+import XCTest
+
+class FlashcardsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
